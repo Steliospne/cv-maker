4d1fcd25a513254b8d18bfcef769a4c70af77bea
Steliospne <63586679+Steliospne@users.noreply.github.com>
2024-09-16 17:48:55 +0200

    Holds the data that populates the components and store the users data

---
+++ pages/data.js
@@ -0,0 +1,15 @@
+const data = [
+  {
+    image: "",
+    contact: {
+      name: { type: "text", value: "John Doe" },
+      phone: { type: "tel", value: "6915253458" },
+      email: { type: "email", value: "example@domain.com" },
+    },
+    education: [],
+    work: [],
+    skills: [],
+  },
+];
+
+export default data;
