5fbed8be13e7af27e1454bf00b2d4937010845dc
Steliospne <63586679+Steliospne@users.noreply.github.com>
2024-09-16 17:47:24 +0200

    Main app file that calls all components and hold the central functionality

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,211 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { useState, useEffect } from "react";
+import Contact from "./components/contact";
+import Avatar from "./components/avatar";
+import SidePanel from "./components/sidepanel";
+import Education from "./components/education";
+import data from "./data";
+import { v4 as uuidv4 } from "uuid";
+import EditLogo from "./components/logo/editLogo";
+import SaveLogo from "./components/logo/saveLogo";
+import Work from "./components/work";
+import Skills from "./components/skills";
 
 export default function Home() {
+  const [isEditable, setIsEditable] = useState(false);
+  const [person, setPerson] = useState(data[0]);
+  useEffect(() => {
+    const examplePerson = data[0];
+    const personExists = window.localStorage.getItem("person");
+
+    if (personExists) {
+      const personLocal = JSON.parse(localStorage.getItem("person"));
+      return setPerson(personLocal);
+    } else {
+      setPerson(examplePerson);
+    }
+  }, []);
+
+  // ===== General Handlers =====
+
+  function handleEditBtn() {
+    setIsEditable(!isEditable);
+    window.localStorage.setItem("person", JSON.stringify(person));
+  }
+
+  // ===== Add Handlers =====
+
+  function handleAddEduFIeld() {
+    setPerson({
+      ...person,
+      education: [
+        ...person.education,
+        {
+          id: uuidv4(),
+          degreeTitle: { type: "text", value: "Degree Title" },
+          instituteName: { type: "text", value: "University/College Name" },
+          startPeriod: { type: "date", value: "" },
+          endPeriod: { type: "date", value: "" },
+        },
+      ],
+    });
+  }
+
+  function handleAddWorkFIeld() {
+    setPerson({
+      ...person,
+      work: [
+        ...person.work,
+        {
+          id: uuidv4(),
+          jobTitle: { type: "text", value: "Job Title" },
+          companyName: { type: "text", value: "Company Name" },
+          description: {
+            type: "textarea",
+            value:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, reiciendis nobis eligendi dignissimos adipisci debitis alias voluptatibus, provident culpa quas corrupti soluta minus aperiam fugit, quidem fugiat assumenda deserunt aliquid.",
+          },
+          startPeriod: { type: "date", value: "" },
+          endPeriod: { type: "date", value: "" },
+        },
+      ],
+    });
+  }
+
+  function handleAddSkill(value) {
+    value !== "" &&
+      setPerson({
+        ...person,
+        skills: [...person.skills, { id: uuidv4(), value: value }],
+      });
+  }
+
+  // ===== On Change Handlers =====
+
+  function handleContactChange(e) {
+    setPerson({
+      ...person,
+      contact: {
+        ...person.contact,
+        [e.target.name]: {
+          ...person.contact[e.target.name],
+          value: e.target.value,
+        },
+      },
+    });
+  }
+
+  function handleEduInputChange(e, index) {
+    setPerson({
+      ...person,
+      education: person.education.map((edu, i) =>
+        i === index
+          ? {
+              ...edu,
+              [e.target.name]: {
+                ...edu[e.target.name],
+                value: e.target.value,
+              },
+            }
+          : edu
+      ),
+    });
+  }
+
+  function handleWorkInputChange(e, index) {
+    setPerson({
+      ...person,
+      work: person.work.map((workInputField, i) =>
+        i === index
+          ? {
+              ...workInputField,
+              [e.target.name]: {
+                ...workInputField[e.target.name],
+                value: e.target.value,
+              },
+            }
+          : workInputField
+      ),
+    });
+  }
+
+  // ===== Delete Handlers =====
+
+  function handleDeleteEduField(e) {
+    setPerson({
+      ...person,
+      education: person.education.filter((field) => field.id !== e.target.id),
+    });
+  }
+
+  function handleDeleteWorkField(e) {
+    setPerson({
+      ...person,
+      work: person.work.filter((field) => field.id !== e.target.id),
+    });
+  }
+
+  function handleDeleteSkill(arr) {
+    setPerson({
+      ...person,
+      skills: person.skills.filter(
+        (skill) => !arr.find((skillToDelete) => skill.id === skillToDelete.id)
+      ),
+    });
+  }
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      const newPerson = { ...person, image: imageUrl };
+      setPerson(newPerson);
+      window.localStorage.setItem("person", JSON.stringify(newPerson));
+    }
+  };
+
+  const handleDeleteBtn = () => {
+    setPerson({ ...person, image: null });
+  };
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
-        <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="https://nextjs.org/icons/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol>
-            <li>
-              Get started by editing <code>pages/index.js</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="https://nextjs.org/icons/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
-          </div>
-        </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
-      </div>
+      <button className='editBtn' type='submit' onClick={handleEditBtn}>
+        {isEditable ? <SaveLogo /> : <EditLogo />}
+      </button>
+      <SidePanel>
+        <Avatar
+          person={person}
+          isEditable={isEditable}
+          onUpload={handleImageUpload}
+          onClick={handleDeleteBtn}
+        />
+        <Contact
+          data={person.contact}
+          isEditable={isEditable}
+          onClick={handleEditBtn}
+          onChange={handleContactChange}
+        />
+        <Education
+          data={person.education}
+          isEditable={isEditable}
+          onChange={handleEduInputChange}
+          onAdd={handleAddEduFIeld}
+          onDelete={handleDeleteEduField}
+        />
+        <Skills
+          data={person.skills}
+          isEditable={isEditable}
+          onAdd={handleAddSkill}
+          onDelete={handleDeleteSkill}
+        />
+      </SidePanel>
+      <h1 className='name'>{person.contact.name.value}</h1>
+      <Work
+        data={person.work}
+        isEditable={isEditable}
+        onChange={handleWorkInputChange}
+        onAdd={handleAddWorkFIeld}
+        onDelete={handleDeleteWorkField}
+      />
     </>
   );
 }
