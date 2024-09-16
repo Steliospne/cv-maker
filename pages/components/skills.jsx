87426d93051d994339cfe5c36a9a6f239ebd8bb9
Steliospne <63586679+Steliospne@users.noreply.github.com>
2024-09-16 17:30:47 +0200

    Adds the skills component

---
+++ pages/components/skills.jsx
@@ -0,0 +1,58 @@
+import { Fragment, useState } from "react";
+import style from "./skills.module.css";
+
+export default function Skills({ data, onAdd, onDelete, isEditable }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const existingData = data.length !== 0 && data;
+
+  let toDelete = [];
+  function handleCheckedBoxes(e) {
+    e.target.checked
+      ? toDelete.push(e.target)
+      : (toDelete = toDelete.filter((item) => item.id !== e.target.id));
+  }
+
+  return (
+    <div className={style.skills}>
+      <h1>Skills</h1>
+      <ul>
+        {existingData &&
+          existingData.map((skill, i) => (
+            <div key={skill.id}>
+              <li>{skill.value}</li>
+              {isEditable && (
+                <input
+                  type='checkbox'
+                  id={skill.id}
+                  onChange={handleCheckedBoxes}
+                />
+              )}
+            </div>
+          ))}
+      </ul>
+      {isEditable && (
+        <>
+          <input
+            type='text'
+            value={inputValue}
+            maxLength='10'
+            placeholder='Type a skill...'
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div className={style.btnWrapper}>
+            <button
+              onClick={() => {
+                onAdd(inputValue);
+                setInputValue("");
+              }}
+            >
+              +
+            </button>
+            <button onClick={() => onDelete(toDelete)}>-</button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
